74274c680eaf01ba9ee3108b492b00d96d74e099
Namoda <nidinawo@gmail.com>
2024-04-23 14:20:38 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-teal-600 text-white text-2xl font-medium text-center py-3 shadow-xl">
+      
+    </div>
+  );
+};
+
+export default Footer;
